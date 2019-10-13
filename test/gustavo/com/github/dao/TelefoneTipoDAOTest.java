53a47cc3135656eb02f37969c219cdacef536d10
Gustavo Maciel <gustav2015@gmail.com>
2019-10-13 02:18:42 -0300

    JUnit Test Case Adicionado

---
+++ test/gustavo/com/github/dao/TelefoneTipoDAOTest.java
@@ -0,0 +1,34 @@
+package gustavo.com.github.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TelefoneTipoDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testListarTelefoneTipo() {
+		fail("Not yet implemented");
+	}
+
+}
