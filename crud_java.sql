c161d93ec40b19fa78920ef7899e1e7201958465
Gustavo Maciel <gustav2015@gmail.com>
2019-10-13 00:05:09 -0300

    Adicionar Script SQL

---
+++ crud_java.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 12-Out-2019 às 22:07
+-- Versão do servidor: 5.7.24
+-- versão do PHP: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_java`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `telefone_tipo`
+--
+
+DROP TABLE IF EXISTS `telefone_tipo`;
+CREATE TABLE IF NOT EXISTS `telefone_tipo` (
+  `id_telefone_tipo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_telefone_tipo`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `telefone_tipo`
+--
+
+INSERT INTO `telefone_tipo` (`id_telefone_tipo`, `nome`) VALUES
+(1, 'Residencial'),
+(2, 'Celular'),
+(3, 'Comercial'),
+(4, 'Fax'),
+(5, 'Não Definido'),
+(6, 'Tipo 0800');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_telefone`
+--
+
+DROP TABLE IF EXISTS `usuario_telefone`;
+CREATE TABLE IF NOT EXISTS `usuario_telefone` (
+  `id_usuario` int(11) NOT NULL,
+  `ddd` int(11) NOT NULL,
+  `numero_telefone` varchar(20) NOT NULL,
+  `id_telefone_tipo` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
